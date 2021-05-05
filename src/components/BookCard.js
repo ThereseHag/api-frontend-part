93b697812d44a06b76474d7223b540c4d7c0f573
unknown <tezzinthecity@gmail.com>
2021-05-06 00:42:35 +0200

    add image to each book card, center content of card

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
 
-const BookCard = ({ title, release_year, series, bookID, series_order, main_characters }) => {
+const BookCard = ({ title, release_year, series, bookID, series_order, main_characters, img }) => {
     return (
       <>
           <div className="book-details">
+            <img src={img} alt="book cover" />
             <h2>{title}</h2>
-            <p>Released: {release_year}</p>
-            <p>Belongs to serie: {series}</p>
-            <p>Order in serie: {series_order}</p>
-            <p>Main characters: {main_characters}</p>
+            <p><b>Released:</b> {release_year}</p>
+            <p><b>Belongs to serie:</b> {series}</p>
+            <p><b>Order in serie:</b> {series_order}</p>
+            <p><b>Main characters:</b> {main_characters}</p>
           </div>
 
           </>
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,9 +4,9 @@ const MainPage = () => {
     return (
         <div className='main-container'>
             <div className='info-part'>
-                <p className='info-text'>Margit Sandemo was a Swedish/Norwegian author who between the years of 1978 and 2018 wrote 185 books.</p>
+                <p className='info-text'>Margit Sandemo was a Swedish/Norwegian author who between the years of 1964 and 2018 wrote 185 books.</p>
                 <p className='info-text'>She may be most famous for the serie Sagan om Isfolket, but she has also written several other series, such as Legenden om Ljusets Rike, Häxmästaren, Trollrunor and Sandemoserien.</p>
-                <p className='info-text'>Find below all books written by Margit.</p>
+                <p className='info-text'>Find below all books written by Margit. You will also find a link to Storytel, where you can read or listen to the books. However, there are of course many other places where you can find them as well.</p>
             </div>
         </div>
     )
