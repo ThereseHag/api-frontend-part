52bd7255c26db73189f1450908e4c8bd7d097861
unknown <tezzinthecity@gmail.com>
2021-05-06 23:24:32 +0200

    add media queries, change font size

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -10,10 +10,10 @@ const BookCard = ({ title, release_year, series, bookID, series_order, main_char
             
             <img className="cover-img" src={img} alt="book cover" />
             <h2>{title}</h2>
-            <p><b>Released:</b> {release_year}</p>
-            <p><b>Belongs to serie:</b> {series}</p>
-            <p><b>Order in serie:</b> {series_order}</p>
-            <p><b>Main characters:</b> {main_characters}</p>
+            <p><b>Publicerad:</b> {release_year}</p>
+            <p><b>Hör till serie:</b> {series}</p>
+            <p><b>Boken har plats i serien:</b> {series_order}</p>
+            <p><b>Huvudpersoner:</b> {main_characters}</p>
           </div>
           </Link>
 
